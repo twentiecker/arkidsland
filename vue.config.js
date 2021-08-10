acc1c9bb6c904858f927145b65348ee6052cb0a4
twentiecker <twentiecker@gmail.com>
2021-08-10 15:15:35 +0700

    install bootstrap

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// change "/arkidsland/" to your subdomain path
+// if you dont use subdomain, just change "/crud/" to "/"
+
+module.exports = {
+  // with subdomain
+  publicPath: process.env.NODE_ENV === "production" ? "/arkidsland/" : "/",
+
+  // without subdomain (example)
+  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+};
